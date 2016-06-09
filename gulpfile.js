3abb7da04696935400a3a687dcb9e0527f329a08
granevich <granevich@gmail.com>
2016-06-09 16:46:17 +0300

    added the comments to undestand what going on.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,19 @@ var gulp = require('gulp'),
     mainBowerFiles = require('main-bower-files'),
     autoprefixer = require('gulp-autoprefixer'),
     csscomb = require('gulp-csscomb');
+    //     sass        = require('gulp-sass'),//переводит sass в css
+    // browserSync = require('browser-sync'),//обновление браузера
+    // concat  = require('gulp-concat'),//сборщик всех файлов
+    // uglify  = require('gulp-uglifyjs'),//минифицирует скрипты
+    // cssnano = require('gulp-cssnano'),//минифицирует css
+    // rename = require('gulp-rename'),//переиминовывает файлы
+    // del = require('del'),//удаляет файлы и директории
+    // imagemin = require('gulp-imagemin'),//минифицирует картинки
+    // pngquant = require('imagemin-pngquant'),//минифицирует png
+    // cache = require('gulp-cache'),//кеширует файлы
+    // autoprefixer = require('gulp-autoprefixer'),//вставляет префиксы
+    // uncss = require('gulp-uncss'),//убирает неиспользующие стили
+    // csscomb = require('gulp-csscomb');//комбинирует красиво стили
   
 
 gulp.task('mainBowerFiles', function () {
