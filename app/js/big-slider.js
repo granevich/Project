f4da76fc4ef4a5c0a6d9b5db56c1bafab4ec00d0
Alex <granevich@gmail.com>
2017-10-16 22:35:47 +0300

    one

---
+++ app/js/big-slider.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('.big-slider').slick({
+        dots: true,
+        arrows:true
+    });
+
+});
--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,6 @@
 /**
  * Created by granevich on 06.05.2016.
  */
+$(document).ready(function () {
 
+});
